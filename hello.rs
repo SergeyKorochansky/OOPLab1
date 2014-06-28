a03d4e5958eb1561d36ef9aaccb0b152bddba3c6
Serhij Korocahnskyj <webzepter@gmail.com>
2014-06-28 07:27:21 +0300

    Add input and calculation

--- hello.rs
+++ hello.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::str;
+fn output(name: ~str, a: int, b: int) {
+    println!("Hello, {}.", name);
+    println!("{} + {} = {}", a, b, a+b);
+}
+
 fn main() {
-	println!("Hello world!");
+    print!("Enter your name: ");
+    let name_raw = io::stdin().read_line().ok().unwrap();
+    let name = str::replace(name_raw, "\n", "");
+
+    print!("Enter a: ");
+    let a_raw = io::stdin().read_line().ok().unwrap();
+    let a_fixed = str::replace(a_raw, "\n", "");
+    let a: int = from_str(a_fixed).unwrap();
+
+    print!("Enter b: ");
+    let b_raw = io::stdin().read_line().ok().unwrap();
+    let b_fixed = str::replace(b_raw, "\n", "");
+    let b: int = from_str(b_fixed).unwrap();
+
+    output(name, a, b);
 }
